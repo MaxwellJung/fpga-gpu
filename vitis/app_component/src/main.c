dee3eb3393130e091b52bf9883a8b86a56b1a6c6
Maxwell Jung <jungmaxwell@gmail.com>
2024-11-30 05:36:56 -0800

    feat: cycle between 3 backgrounds

--- vitis/app_component/src/main.c
+++ vitis/app_component/src/main.c
@@ -13,6 +13,10 @@
 
 #define BRAM_DEVICE_ID      XPAR_XBRAM_0_BASEADDR
 
+void delay(u32 d);
+void draw_diagonal_background(XBram_Config *frame_buffer_cfg_ptr);
+void draw_horizontal_background(XBram_Config *frame_buffer_cfg_ptr);
+void draw_vertical_background(XBram_Config *frame_buffer_cfg_ptr);
 
 int main() {
 	xil_printf("Entered function main\r\n");
@@ -41,25 +45,23 @@ int main() {
     bram_cfg_ptr = XBram_LookupConfig(BRAM_DEVICE_ID);
     XBram_CfgInitialize(&bram_device, bram_cfg_ptr, bram_cfg_ptr->CtrlBaseAddress);
 
-    int i = 0;
-    for (int row = 0; row < 300; row++) {
-        for (int col = 0; col < 400; col++) {
-            i = 400 * row + col;
-            XBram_Out8(bram_cfg_ptr->BaseAddress + i, row + col);
-            // XBram_Out8(bram_cfg_ptr->BaseAddress + i, 0);
-            // xil_printf("Wrote %x to address %x\r\n", i, bram_cfg_ptr->BaseAddress + i);
-            // for (u32 delay = 0; delay < 1000; delay++);
-        }
-    }
-
+    draw_diagonal_background(bram_cfg_ptr);
 
 	// u32 sw_data;
     u32 pixel_index = 0;
 	u8 pixel_color;
+    int f = 15;
 	while (1) {
 		// sw_data = XGpio_DiscreteRead(&sw_device, SW_CHANNEL);
 		// XGpio_DiscreteWrite(&led_device, LED_CHANNEL, sw_data);
         // pixel_index = sw_data;
+        if (pixel_index%f < f/3) {
+            draw_horizontal_background(bram_cfg_ptr);
+        } else if (pixel_index%f < 2*f/3) {
+            draw_diagonal_background(bram_cfg_ptr);
+        } else {
+            draw_vertical_background(bram_cfg_ptr);
+        }
 
 		pixel_color = XBram_In8(bram_cfg_ptr->BaseAddress + pixel_index);
 		XBram_Out8(bram_cfg_ptr->BaseAddress + pixel_index, 0xff);
@@ -69,4 +71,47 @@ int main() {
         pixel_index++;
         if (pixel_index > 400*300) pixel_index = 0;
 	}
+}
+
+void delay(u32 d) {
+    for (u32 i = 0; i < d; i++);
+}
+
+void draw_diagonal_background(XBram_Config *frame_buffer_cfg_ptr) {
+    int pixel_index = 0;
+    for (int row = 0; row < 300; row++) {
+        for (int col = 0; col < 400; col++) {
+            pixel_index = 400 * row + col;
+            XBram_Out8(frame_buffer_cfg_ptr->BaseAddress + pixel_index, row + col);
+            // XBram_Out8(bram_cfg_ptr->BaseAddress + i, 0);
+            // xil_printf("Wrote %x to address %x\r\n", i, bram_cfg_ptr->BaseAddress + i);
+            // for (u32 delay = 0; delay < 1000; delay++);
+        }
+    }
+}
+
+void draw_horizontal_background(XBram_Config *frame_buffer_cfg_ptr) {
+    int pixel_index = 0;
+    for (int row = 0; row < 300; row++) {
+        for (int col = 0; col < 400; col++) {
+            pixel_index = 400 * row + col;
+            XBram_Out8(frame_buffer_cfg_ptr->BaseAddress + pixel_index, row);
+            // XBram_Out8(bram_cfg_ptr->BaseAddress + i, 0);
+            // xil_printf("Wrote %x to address %x\r\n", i, bram_cfg_ptr->BaseAddress + i);
+            // for (u32 delay = 0; delay < 1000; delay++);
+        }
+    }
+}
+
+void draw_vertical_background(XBram_Config *frame_buffer_cfg_ptr) {
+    int pixel_index = 0;
+    for (int row = 0; row < 300; row++) {
+        for (int col = 0; col < 400; col++) {
+            pixel_index = 400 * row + col;
+            XBram_Out8(frame_buffer_cfg_ptr->BaseAddress + pixel_index, col);
+            // XBram_Out8(bram_cfg_ptr->BaseAddress + i, 0);
+            // xil_printf("Wrote %x to address %x\r\n", i, bram_cfg_ptr->BaseAddress + i);
+            // for (u32 delay = 0; delay < 1000; delay++);
+        }
+    }
 }
